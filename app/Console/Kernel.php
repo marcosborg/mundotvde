91c056520fe552cceb928f15ccac8efa2d10b3ea
marcosborg <m.borges.mail@gmail.com>
2023-04-26 15:27:10 +0100

    Update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Artisan;
 
 class Kernel extends ConsoleKernel
 {
@@ -15,7 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('calendar:check')->everyMinute();
     }
 
     /**
@@ -25,7 +26,11 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
+
+        Artisan::command('calendar:check', function () {
+            Artisan::call("calendar:send");
+        });
 
         require base_path('routes/console.php');
     }
