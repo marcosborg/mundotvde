2df985467918c5bd8dd3ade1091650f837f42888
marcosborg <m.borges.mail@gmail.com>
2023-10-12 11:05:08 +0100

    Update

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,9 @@
 <?php
+use App\Models\StandCar;
 
 Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', 'middleware' => ['auth:sanctum']], function () {
+});
+
+Route::get('stand-cars', function () {
+    return StandCar::all();
 });
