86b922e6ce0057f313c64ed216b97dd147e0b0e9
marcosborg <m.borges.mail@gmail.com>
2023-10-12 19:25:04 +0100

    Update API

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,8 @@
 <?php
+use App\Models\Brand;
+use App\Models\CarModel;
+use App\Models\Fuel;
+use App\Models\Origin;
 use App\Models\StandCar;
 
 Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', 'middleware' => ['auth:sanctum']], function () {
@@ -13,4 +17,33 @@ Route::get('stand-cars', function () {
         'origin',
         'status',
     ]);
+});
+
+Route::get('stand-car/{id}', function ($id) {
+    return StandCar::find($id)->load([
+        'brand',
+        'car_model',
+        'fuel',
+        'month',
+        'origin',
+        'status',
+    ]);
+});
+
+Route::get('filter-elements', function () {
+    $cars = StandCar::all();
+    return [
+        'brands' => Brand::all(),
+        'models' => CarModel::all(),
+        'fuels' => Fuel::all(),
+        'origins' => Origin::all(),
+        'kilometers' => [
+            'min' => $cars->min('kilometers'),
+            'max' => $cars->max('kilometers')
+        ],
+        'prices' => [
+            'min' => $cars->min('price'),
+            'max' => $cars->max('price')
+        ]
+    ];
 });
