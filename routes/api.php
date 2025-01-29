afcdc24357eed772c42be0c7c7f27112f9a40c3b
marcosborg <m.borges.mail@gmail.com>
2025-01-29 15:15:44 +0000

    Move PDF report route to a new endpoint for better organization

--- routes/api.php
+++ routes/api.php
@@ -86,10 +86,11 @@ Route::prefix('app')->middleware(['auth:sanctum'])->group(function () {
     Route::get('admin', 'Api\\AppController@admin');
     Route::get('my-receipts', 'Api\\AppController@myReceipts');
     Route::get('reports', 'Api\\AppController@reports');
-    Route::get('reports/pdf/{activity_launch_id}', 'Api\\AppController@pdf');
+    
     Route::get('documents', 'Api\\AppController@documents');
     Route::post('send-receipt', 'Api\\AppController@sendReceipt');
     Route::get('my-documents', 'Api\\AppController@myDocuments');
     Route::post('send-document', 'Api\\AppController@sendDocument');
 });
 
+Route::get('app/reports/pdf/{activity_launch_id}', 'Api\\AppController@pdf');
