eb6f8134762d095a6b542b417921fead2ddcc4f4
marcosborg <m.borges.mail@gmail.com>
2025-09-01 09:49:27 +0100

    Remove redundant notification for activity launches in data method

--- app/Http/Controllers/Admin/PayoutsToDriversController.php
+++ app/Http/Controllers/Admin/PayoutsToDriversController.php
@@ -112,7 +112,6 @@ class PayoutsToDriversController extends Controller
         $activityLaunche->total_descount_after_taxes = $activityLaunche->sub + $activityLaunche->taxes;
 
         $activityLaunche->driver->user->notify(new ActivityLaunchesSend($activityLaunche));
-        User::find(1)->notify(new ActivityLaunchesSend($activityLaunche));
     }
 
     private function getBalance($driver_id)
